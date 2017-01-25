9b4306c91c4ba9e4f009ff90cea36004f0130051
Andri Snær Didriksen Ásmundsson <andrias303@nemi.tskoli.is>
2017-01-25 14:48:39 +0000

    gerði línu sem er sett inn í skjalið og svo er því lokað

--- github-gso-afangaverkefni.py
+++ github-gso-afangaverkefni.py
@@ -5,3 +5,7 @@ skjalanafn = input("Sláðu inn heiti fyrir skránna þína")
 skjalanafnid = skjalanafn+ ".txt"
 minskra=open(skjalanafnid, "w+")
 
+stuff = input("Sláðu inn texta")
+minskra.write(stuff+"\n")
+minskra.close
+
